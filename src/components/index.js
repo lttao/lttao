2bed15becdf7c1ad17c495c2adc7be6a80fdcef9
LuoTao <799204419@qq.com>
2020-04-20 18:19:17 +0800

    增加npm发布配置

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import bgMask from "./bg-mask/index";
+import loading from "./loading";
+
+const components = {
+  bgMask,
+  loading
+}
+
+export default components;
--- src/views/About.vue
+++ src/views/About.vue
@@ -9,46 +9,60 @@
       <checkbox :name="2">select</checkbox>
     </checkbox-group>
     <div>{{ checkboxValue }}</div>
-    <div @click="showLoading">加载</div>
-    <div @click="hideLoading">隐藏</div>
-    <h1 @click="showToast" style="position: relative; z-index: 9;">This is an about page</h1>
+    <div @click="showLoading">显示加载</div>
+    <div @click="hideLoading">隐藏加载</div>
+    <div @click="showTimeLoading">显示加载（2s自动关闭）</div>
+    <h1 @click="showToast" style="position: relative; z-index: 9;">显示toast</h1>
+    <slider v-model="sliderValue"></slider>
+    <div>{{ sliderValue }}</div>
   </div>
 </template>
 
 <script>
-
-  import checkboxGroup from  "@/components/checkbox-group";
+import checkboxGroup from "@/components/checkbox-group";
 import checkbox from "@/components/checkbox";
-  import ltInput from "@/components/input";
-  export default {
-    name: "about",
-    components: {
-      checkbox,
-      checkboxGroup,
-      ltInput
+import ltInput from "@/components/input";
+import slider from "@/components/slider/slider";
+export default {
+  name: "about",
+  components: {
+    checkbox,
+    checkboxGroup,
+    ltInput,
+    slider
+  },
+  data() {
+    return {
+      value: "",
+      checked: false,
+      checkboxValue: [1],
+      sliderValue: 0
+    };
+  },
+  methods: {
+    showToast() {
+      this.$toast({
+        msg: "成功"
+      });
     },
-    data() {
-      return {
-        value: '',
-        checked: false,
-        checkboxValue: [1]
-      }
+    showLoading() {
+      this.$loading.show({
+        msg: "加载中..",
+        mask: false
+      });
     },
-    methods: {
-      showToast() {
-        this.$toast({
-          msg: '成功'
-        })
-      },
-      showLoading() {
-        this.$loading.show({
-          msg: "加载中..",
-          mask: false
-        })
-      },
-      hideLoading() {
-        this.$loading.hide()
-      }
+    hideLoading() {
+      this.$loading.hide();
+    },
+    showTimeLoading() {
+      this.$loading.show({
+        msg: "加载中..",
+        mask: true
+      });
+      setTimeout(() => {
+        this.$loading.hide();
+      }, 2000);
     }
-  };
+  }
+};
 </script>
